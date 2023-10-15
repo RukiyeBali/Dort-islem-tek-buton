001523bb49d0bc0993b281809b84ded8282291ef
Rukiye Balı <145977861+RukiyeBali@users.noreply.github.com>
2023-10-15 20:55:08 +0300

    Add files via upload

---
+++ Dort-islem-tek-buton/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Dort_islem_tek_buton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblToplam = new System.Windows.Forms.Label();
+            this.lblFark = new System.Windows.Forms.Label();
+            this.lblCarpim = new System.Windows.Forms.Label();
+            this.lblBolum = new System.Windows.Forms.Label();
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.Location = new System.Drawing.Point(87, 149);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(156, 32);
+            this.btnHesapla.TabIndex = 0;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1.Sayı";
+            // 
+            // lblToplam
+            // 
+            this.lblToplam.AutoSize = true;
+            this.lblToplam.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblToplam.Location = new System.Drawing.Point(12, 218);
+            this.lblToplam.Name = "lblToplam";
+            this.lblToplam.Size = new System.Drawing.Size(72, 20);
+            this.lblToplam.TabIndex = 2;
+            this.lblToplam.Text = "Toplam:";
+            // 
+            // lblFark
+            // 
+            this.lblFark.AutoSize = true;
+            this.lblFark.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblFark.Location = new System.Drawing.Point(120, 218);
+            this.lblFark.Name = "lblFark";
+            this.lblFark.Size = new System.Drawing.Size(50, 20);
+            this.lblFark.TabIndex = 3;
+            this.lblFark.Text = "Fark:";
+            // 
+            // lblCarpim
+            // 
+            this.lblCarpim.AutoSize = true;
+            this.lblCarpim.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCarpim.Location = new System.Drawing.Point(208, 218);
+            this.lblCarpim.Name = "lblCarpim";
+            this.lblCarpim.Size = new System.Drawing.Size(70, 20);
+            this.lblCarpim.TabIndex = 4;
+            this.lblCarpim.Text = "Çarpım:";
+            // 
+            // lblBolum
+            // 
+            this.lblBolum.AutoSize = true;
+            this.lblBolum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBolum.Location = new System.Drawing.Point(313, 218);
+            this.lblBolum.Name = "lblBolum";
+            this.lblBolum.Size = new System.Drawing.Size(64, 20);
+            this.lblBolum.TabIndex = 5;
+            this.lblBolum.Text = "Bölüm:";
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Location = new System.Drawing.Point(123, 65);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(101, 20);
+            this.txtSayi1.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(81, 106);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "2.Sayı";
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Location = new System.Drawing.Point(124, 99);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 322);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtSayi1);
+            this.Controls.Add(this.lblBolum);
+            this.Controls.Add(this.lblCarpim);
+            this.Controls.Add(this.lblFark);
+            this.Controls.Add(this.lblToplam);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnHesapla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblToplam;
+        private System.Windows.Forms.Label lblFark;
+        private System.Windows.Forms.Label lblCarpim;
+        private System.Windows.Forms.Label lblBolum;
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSayi2;
+    }
+}
+
---
+++ Dort-islem-tek-buton/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dort_islem_tek_buton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHeasapla_Click(object sender, EventArgs e)
+
+        {
+            double sayi1, sayi2, toplam, fark, carpim, bolum;
+
+            sayi1 = Convert.ToDouble(txtSayi1.Text);
+            sayi2 = Convert.ToDouble(txtSayi2.Text);
+
+            toplam = sayi1 + sayi2;
+            fark = sayi1 - sayi2;
+            carpim = sayi1 * sayi2;
+            bolum = sayi1 / sayi2;
+
+
+            lblToplam.Text = "Toplam: " + toplam;
+            lblFark.Text = "Fark: " + fark;
+            lblCarpim.Text = "Çarpım: " + carpim;
+            lblBolum.Text = "Bölüm: " + bolum;
+        }
+    }
+}
